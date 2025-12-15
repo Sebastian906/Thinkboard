cf4eadefdc58ae2bb83d34f534da1a4d4516b5f2
Sebastian906 <elsebas1912@gmail.com>
2025-12-14 23:15:48 -0500

    Finalización de HomePage y creación de componente NoteCard y función utils para formato de fecha y hora

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,19 +2,31 @@ import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import axios from 'axios'
+import toast from 'react-hot-toast'
+import NoteCard from '../components/NoteCard'
 
 const HomePage = () => {
-    const [isRateLimited, setIsRateLimited] = useState(true);
+    const [isRateLimited, setIsRateLimited] = useState(false);
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchNotes = async () => {
             try {
-                const res = await axios.post('http://localhost:5001/api/notes');
+                const res = await axios.get('http://localhost:5001/api/notes/');
                 console.log(res.data);
+                setNotes(res.data);
+                setIsRateLimited(false);
             } catch (error) {
                 console.log('Error fetching notes:', error);
+                console.log(error);
+                if (error.response?.status === 429) {
+                    setIsRateLimited(true);
+                } else {
+                    toast.error('Failed to load notes');
+                }
+            } finally {
+                setLoading(false);
             }
         }
         fetchNotes();
@@ -24,6 +36,16 @@ const HomePage = () => {
         <div className='min-h-screen'>
             <Navbar/>
             {isRateLimited && <RateLimitedUI />}
+            <div className='max-w-7xl mx-auto p-4 mt-6'>
+                {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+                {notes.length > 0 && !isRateLimited && (
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                        {notes.map((note) => (
+                            <NoteCard key={note._id} note={note} />
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
